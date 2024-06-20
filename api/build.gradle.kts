e50b6b27b3e5f2f4f07c42a2b2b2b3353b7e22d7
Oleksii PELYKH <oleksii@pelykh.com>
2024-06-20 18:33:32 +0200

    (chore) test coverage

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -16,6 +16,7 @@ plugins {
     `java-library`
     `maven-publish`
     signing
+    jacoco
 }
 
 repositories {
@@ -51,8 +52,18 @@ java {
     withJavadocJar()
 }
 
-tasks.withType<Test> {
+tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required = true
+        html.required = true
+    }
 }
 
 tasks.named<Jar>("sourcesJar") {
