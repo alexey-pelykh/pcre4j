37550922d9862c55500f689bd46e61859af6e9a5
Oleksii PELYKH <oleksii@pelykh.com>
2024-06-22 21:58:16 +0200

    (fix) lib: unmatched groups

--- regex/src/test/java/org/pcre4j/regex/MatcherTests.java
+++ regex/src/test/java/org/pcre4j/regex/MatcherTests.java
@@ -618,4 +618,32 @@ public class MatcherTests {
         assertEquals(javaMatcher.end("two"), pcre4jMatcher.end("two"));
     }
 
+    @Test
+    void unmatchedGroups() {
+        var regex = "42((?<exclamation>!)|(?<question>\\?))";
+        var input = "42!";
+        var javaMatcher = java.util.regex.Pattern.compile(regex).matcher(input);
+        var pcre4jMatcher = Pattern.compile(regex).matcher(input);
+
+        assertEquals(javaMatcher.find(), pcre4jMatcher.find());
+
+        assertEquals(javaMatcher.group(), pcre4jMatcher.group());
+        assertEquals(javaMatcher.group(0), pcre4jMatcher.group(0));
+        assertEquals(javaMatcher.group(1), pcre4jMatcher.group(1));
+        assertEquals(javaMatcher.group(2), pcre4jMatcher.group(2));
+        assertEquals(javaMatcher.groupCount(), pcre4jMatcher.groupCount());
+        assertEquals(javaMatcher.start(), pcre4jMatcher.start());
+        assertEquals(javaMatcher.start(0), pcre4jMatcher.start(0));
+        assertEquals(javaMatcher.start(1), pcre4jMatcher.start(1));
+        assertEquals(javaMatcher.start(2), pcre4jMatcher.start(2));
+        assertEquals(javaMatcher.start("exclamation"), pcre4jMatcher.start("exclamation"));
+        assertEquals(javaMatcher.start("question"), pcre4jMatcher.start("question"));
+        assertEquals(javaMatcher.end(), pcre4jMatcher.end());
+        assertEquals(javaMatcher.end(0), pcre4jMatcher.end(0));
+        assertEquals(javaMatcher.end(1), pcre4jMatcher.end(1));
+        assertEquals(javaMatcher.end(2), pcre4jMatcher.end(2));
+        assertEquals(javaMatcher.end("exclamation"), pcre4jMatcher.end("exclamation"));
+        assertEquals(javaMatcher.end("question"), pcre4jMatcher.end("question"));
+    }
+
 }
